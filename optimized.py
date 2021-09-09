d7eeaf7b06a9e7e11aa51b9dc4dfa3817ddf484f
LudovicDM <ludovic.dematteis@icloud.com>
2021-09-09 15:59:55 +0200

    insert __str__ method - does not insert negative or zero actions

--- optimized.py
+++ optimized.py
@@ -3,6 +3,9 @@ import time
 import pandas as pd
 
 source = './csv/BruteForce.csv'
+dataset1 = './csv/dataset1_Python+P7.csv'
+dataset2 = './csv/dataset2_Python+P7.csv'
+
 WALLET = 500
 sum_coast_max_action = 0
 max_benefit = 0
@@ -21,25 +24,31 @@ class Actions:
 
         self.LIST_ACTIONS.append(self)
 
+    def __str__(self):
+        return f"- {elem.name}"
+
     @classmethod
     def return_list(cls):
         return cls.LIST_ACTIONS
 
 
 # Tri du meilleur rendement au plus mauvais sans prise en compte de la valeur de l'action
-data_sorted = data.sort_values(by='benefit', ascending=False)
+data_sorted = data.sort_values(by='profit', ascending=False)
 
 for action in data_sorted.index:
     if data_sorted['price'][action] < WALLET:
-        Actions(data_sorted['Name'][action], data_sorted['price'][action], data_sorted['benefit'][action])
-        WALLET -= data_sorted['price'][action]
+        if data_sorted['price'][action] <= 0:
+            pass
+        else:
+            Actions(data_sorted['name'][action], data_sorted['price'][action], data_sorted['profit'][action])
+            WALLET -= data_sorted['price'][action]
     else:
-        break
+        pass
 
 final_list = Actions.return_list()
 print("\nAprès analyse, la meilleure combinaison est :")
 for elem in final_list:
-    print(f"- {elem.name}")
+    print(elem)
     sum_coast_max_action += elem.cost
     max_benefit += elem.benefit_euro
 
